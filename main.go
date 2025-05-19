6561b02ef3839cab1258a5e58955757bb5acc94c
Dan <songokura01@mail.ru>
2025-05-19 23:03:53 +0500

    Reading RESP

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
-	"os"
 )
 
 func main() {
+	fmt.Println("Listening on port :6379")
 	l, err := net.Listen("tcp", ":6379")
 	if err != nil {
 		fmt.Println(err)
@@ -22,16 +21,15 @@ func main() {
 	defer conn.Close()
 
 	for {
-		buf := make([]byte, 1024)
-
-		_, err = conn.Read(buf)
+		resp := NewResp(conn)
+		value, err := resp.Read()
 		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			fmt.Println("error rediong from client: ", err.Error())
-			os.Exit(1)
+			fmt.Println(err)
+			return
 		}
+
+		fmt.Println(value)
+
 		conn.Write([]byte("+OK\r\n"))
 	}
 }
